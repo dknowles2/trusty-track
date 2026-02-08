0175641fdb8d9013641c6420f8c8b53d8369905d
David Knowles <dknowles2@gmail.com>
2026-02-08 15:54:53 -0500

    Allow network access to frontend (host: true)

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -29,6 +29,7 @@ export default defineConfig(({ mode }) => {
       }
     ].filter(Boolean),
     server: {
+      host: true,
       port: isHttps ? 5174 : 5173,
       https: isHttps ? {
         key: '../certs/localhost-key.pem',
